63939ebe21961da555d4b3603d19895bc60ae9ce
Connor Lindsey <cblindsey3@gmail.com>
2020-02-05 07:42:16 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Nunito Sans", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: "Nunito Sans", sans-serif;
   }
   body {
     margin: 0;
