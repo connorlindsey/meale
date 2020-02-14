a37baac9e2745c558bcf0b338a61b370406baf80
Chaz <50216139+macildur@users.noreply.github.com>
2020-02-14 10:55:32 -0700

    updates to recipe page

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -19,6 +19,32 @@ const Main = styled.main`
   height: 100%;
 `
 
+const BigRecipeCard = styled.div`
+  box-shadow: ${props => props.theme.elevation1};
+  background-color: ${props => props.theme.grey["100"]};
+  border-radius: ${props => props.theme.borderRadius};
+  max-width: 75%
+
+  h1 {
+    font-size: 40px
+  }
+
+  p {
+    font-weight: 500;
+  }
+
+  img {
+    width: 125px;
+    min-height: 100%;
+    object-fit: cover;
+    margin: 0;
+  }
+`
+
+const Img = styled.img`
+  border-radius: 0 ${props => props.theme.borderRadius} ${props => props.theme.borderRadius} 0;
+`
+
 const RecipePage = () => {
   let { id } = useParams()
   const [recipe, setRecipe] = useState()
@@ -30,7 +56,15 @@ const RecipePage = () => {
   return (
     <Container>
       <Nav />
-      <Main>{recipe && <div>{recipe.name}</div>}</Main>
+      <Main>
+        {recipe && 
+        <BigRecipeCard>
+          <h1>{recipe.name}</h1>
+          <p>{ recipe.description }</p>
+          <p>{ }</p>
+          <Img src={ recipe.image }></Img>
+        </BigRecipeCard>}
+      </Main>
     </Container>
   )
 }
